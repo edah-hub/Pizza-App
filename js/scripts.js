45261b3acdb5ef0697f4fdd3f7cd384430cf208d
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-03-20 11:03:34 +0300

    adds a function to calculate the total amount

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,42 @@ $(document).ready(function () {
         { size: "extraLarge", price: 1200 }
     ];
     Pizza.prototype.toppingsPrices = [
-        { name: "pepperoni", price: 60 },
-        { name: "mushrooms", price: 80 },
-        { name: "onions", price: 50 },
-        { name: "sausage", price: 70 },
-        { name: "extraCheese", price: 100 },
-        { name: "blackOlives", price: 80 },
-        { name: "greenPeppers", price: 80 }
+        { name: "vegtikka", price: 60 },
+        { name: "chickentikka", price: 80 },
+        { name: "periperi", price: 50 },
+        { name: "bbq", price: 70 },
+        { name: "boerewors", price: 100 },
+        { name: "hawaiian", price: 80 },
+        { name: "mushroom", price: 80 },
+        { name: "regina", price: 80 }
     ];
+
+    Pizza.prototype.getTotal = function () {
+        let total = 0;
+
+        if (this.size) {
+            const pizzaSize = this.size;
+
+            const size = Pizza.prototype.pizzaSizePrices.find(function (p) {
+                return p.size === pizzaSize;
+            });
+
+            total = total + size.price;
+        }
+
+        if (this.toppings) {
+            let toppingsTotal = 0;
+            const pizzaToppings = this.toppings;
+
+            for (let i = 0; i < pizzaToppings.length; i++) {
+                const topping = Pizza.prototype.toppingsPrices.find(function (p) {
+                    return p.name === pizzaToppings[i].trim();
+                });
+
+                toppingsTotal = toppingsTotal + topping.price;
+            }
+
+            total = total + toppingsTotal;
+        }
+
+        total = total * this.quantity;
